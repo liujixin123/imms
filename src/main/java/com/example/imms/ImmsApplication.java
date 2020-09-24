56811a282c2cad9f0db205071e6e47bda902a3f4
caochengmei.hf <caochengmei.hf@cabletech.com.cn>
2020-09-24 16:35:26 +0800

    初始导入

--- src/main/java/com/example/imms/ImmsApplication.java
+++ src/main/java/com/example/imms/ImmsApplication.java
@@ -19,6 +19,8 @@ public class ImmsApplication  {
 		SpringApplication.run(ImmsApplication.class, args);
 	}
 
+
+
 //	@Override
 //	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
